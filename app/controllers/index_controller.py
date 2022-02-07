feed685b726f09565746e552889432ec12392e06
Kelly Wu <kaw42@njit.edu>
2022-02-07 17:29:23 -0500

    Changed the name in the controller

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Kelly Wu"
         return render_template('index.html', name=name)
