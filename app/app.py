3e830d3a14f59c0f62b2ebcb8f7b5e25b9a29472
kaw42 <kaw42@njit.edu>
2022-03-05 22:56:34 -0500

    10:56pm 3/5 docker,git, python html works

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from controllers.index_controller import IndexController
 from werkzeug.debug import DebuggedApplication
 from controllers.git_controller import GitController
 from controllers.docker_controller import DockerController
-
+from controllers.python_controller import PythonController
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -23,3 +23,7 @@ def git_get():
 @app.route("/docker", methods=['GET'])
 def docker_get():
     return DockerController.get()
+
+@app.route("/python", methods=['GET'])
+def python_get():
+    return PythonController.get()
