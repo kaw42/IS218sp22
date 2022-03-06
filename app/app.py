e5f3bf88c28a9766efba3af30965bfe737208e28
kaw42 <kaw42@njit.edu>
2022-03-05 22:52:28 -0500

    10:51pm 3/5 docker and git html works

--- app/app.py
+++ app/app.py
@@ -1,12 +1,10 @@
 """A simple flask web app"""
 from flask import Flask, render_template
 
-from IS218sp22.app.controllers.cicd_controller import CicdController
-from IS218sp22.app.controllers.docker_controller import DockerController
-from IS218sp22.app.controllers.python_controller import PythonController
 from controllers.index_controller import IndexController
 from werkzeug.debug import DebuggedApplication
 from controllers.git_controller import GitController
+from controllers.docker_controller import DockerController
 
 
 app = Flask(__name__)
@@ -25,11 +23,3 @@ def git_get():
 @app.route("/docker", methods=['GET'])
 def docker_get():
     return DockerController.get()
-
-@app.route("/python", methods=['GET'])
-def python_get():
-    return PythonController.get()
-
-@app.route("/cicd", methods=['GET'])
-def cicd_get():
-    return CicdController.get()
--- app/controllers/git_controller.py
+++ app/controllers/git_controller.py
@@ -1,7 +1,6 @@
 from flask import render_template
 #don't change this import or it can't find the module controller
-from  . controller import ControllerBase
-
+from . controller import ControllerBase
 
 class GitController(ControllerBase):
     @staticmethod
