b9f0037957dba2b3c1309bcbbdb9bf9b1d7ad089
kaw42 <kaw42@njit.edu>
2022-03-05 23:22:06 -0500

    11:21pm 3/5 all pages work

--- app/app.py
+++ app/app.py
@@ -6,6 +6,7 @@ from werkzeug.debug import DebuggedApplication
 from controllers.git_controller import GitController
 from controllers.docker_controller import DockerController
 from controllers.python_controller import PythonController
+from controllers.cicd_controller import CicdController
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -27,3 +28,7 @@ def docker_get():
 @app.route("/python", methods=['GET'])
 def python_get():
     return PythonController.get()
+
+@app.route("/cicd", methods=['GET'])
+def cicd_get():
+    return CicdController.get()
